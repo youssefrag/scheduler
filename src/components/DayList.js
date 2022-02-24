a4aa476653005acd251674c27a3d8af90a054c3a
Youssef Ragab <ragab.youssef95@gmail.com>
2022-02-23 21:54:58 -0500

    Adding components to storybook

---
+++ src/components/DayList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import DayListItem from './DayListItem'
+
+export default function DayList(props) {
+  const dayListItems = props.days.map(day => 
+  <DayListItem 
+    key={day.id} 
+    name={day.name} 
+    spots={day.spots} 
+    selected={props.name === props.value} 
+    setDay={props.onChange} 
+  />)
+
+  return(
+    <ul>
+      {dayListItems}
+    </ul>
+  )
+}
