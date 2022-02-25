eaa1dc46fd9ac5bac2b7719abb4f16636384e1ca
Youssef Ragab <ragab.youssef95@gmail.com>
2022-02-25 14:33:35 -0500

    added nested promise

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,65 +6,38 @@ import "components/Application.scss";
 
 import DayList from "./DayList"
 import Appointment from "./Appointment"
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
+import { getAppointmentsForDay } from 'helpers/selectors'
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // appointments: {}
+    appointments: {},
+    interviewers: {},
   });
 
-  const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  const dailyAppointments = [];
+
+
+
+
+
+  const setDay = day => {
+    setState({...state, day})
+  };
 
   useEffect(() => {
-    const daysURL = 'http://localhost:8001/api/days'
-    axios.get(daysURL).then(response => {
-      setDays([...response.data])
-    })
-  }, [])
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers')
+    ]).then(([{data: days}, {data: appointments}, {data: interviewers}]) => 
+      setState(prev => ({...prev, days, appointments, interviewers})))}
+  , [])
 
-  const listSchedule = appointments.map((appointment) => {
+
+  const listSchedule = getAppointmentsForDay(state, state.day).map((appointment) => {
+    console.log(appointment)
     return(
       <Appointment key={appointment.id} {...appointment} />
     )
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,12 +2,13 @@ import React from "react";
 import DayListItem from './DayListItem'
 
 export default function DayList(props) {
+
   const dayListItems = props.days.map(day => 
   <DayListItem 
     key={day.id} 
     name={day.name} 
     spots={day.spots} 
-    selected={props.name === props.value} 
+    selected={day.name === props.value} 
     setDay={props.onChange} 
   />)
 
